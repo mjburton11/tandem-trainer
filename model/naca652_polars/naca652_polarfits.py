001745735fd1b20fda56a316f522d3369e2e8f68
mjburton <mjburton@mit.edu>
2016-12-10 16:21:00 -0500

    generate polarfit for naca652

--- model/naca652_polars/naca652_polarfits.py
+++ model/naca652_polars/naca652_polarfits.py
@@ -1,7 +1,8 @@
-"jho1_polarfits.py"
+"naca652_polarfits.py"
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from gpfit.fit import fit
 
 def text_to_df(filename):
     "parse XFOIL polars and concatente data in DataFrame"
@@ -31,7 +32,7 @@ def fit_setup(Re_range):
     CD = []
     RE = []
     for r in Re_range:
-        dataf = text_to_df("jho1.ncrit09.Re%dk.pol" % r)
+        dataf = text_to_df("naca652.ncrit09.Re%dk.pol" % r)
         CL.append(dataf["CL"])
         CD.append(dataf["CD"])
         RE.append([r*1000.0]*len(dataf["CL"]))
@@ -48,12 +49,10 @@ def fit_setup(Re_range):
     return x, y
 
 def return_fit(cl, re):
-    "polar fit for the JHO1 airfoil"
-    cd = (0.0247*cl**2.49*re**-1.11 + 2.03e-7*cl**12.7*re**-0.338 +
-          6.35e10*cl**-0.243*re**-3.43 + 6.49e-6*cl**-1.9*re**-0.681)**(1/3.72)
-    # cd = (0.0247*cl**2.5*re**-1.1 + 2.03e-7*cl**13*re**-0.34 +
-    #       6.35e10*cl**-0.24*re**-3.4 + 6.49e-6*cl**-1.9*re**-0.68)**(1/3.7)
-    # SMA function, K=3, max RMS error = 0.00489
+    "polar fit for the naca652 airfoil, K=3, SMA, RMS=0.0714"
+    cd = (2.44337e-49 * (cl)**25.264 * (re)**2.09928
+          + 1.15012e+56 * (cl)**92.7062 * (re)**-14.1311
+          + 1.56299e-10 * (cl)**0.0617453 * (re)**-5.1536)**(1./18.3487)
     return cd
 
 def plot_fits(re):
@@ -63,7 +62,7 @@ def plot_fits(re):
     fig, ax = plt.subplots()
     cls = np.linspace(0.2, 1.3, 20)
     for r, col in zip(re, colors):
-        dataf = text_to_df("jho1.ncrit09.Re%dk.pol" % r)
+        dataf = text_to_df("naca652.ncrit09.Re%dk.pol" % r)
         ax.plot(dataf["CL"], dataf["CD"], "o", c=col)
         cd = return_fit(cls, r*1000.)
         ax.plot(cls, cd, c=col, label="Re = %dk" % r)
@@ -74,7 +73,8 @@ def plot_fits(re):
     return fig, ax
 
 if __name__ == "__main__":
-    Re = range(200, 750, 50)
-    X, Y = fit_setup(Re) # call fit(X, Y, 4, "SMA") to get fit
-    F, A = plot_fits([300, 350, 400, 450, 500])
-    F.savefig("jho1polarfit1.pdf")
+    Re = range(1000, 4500, 500)
+    X, Y = fit_setup(Re)
+    Cn, Rm = fit(X, Y, 3, "SMA")
+    F, A = plot_fits([1500, 2000, 2500, 3000, 3500])
+    F.savefig("naca652polarfit1.pdf")
